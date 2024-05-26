e08d225bd57c06e4b3f02e46eb9f9526ea46e2a1
Alejandro Subero <alejandrosubero.ar@gmail.com>
2024-05-26 01:49:30 -0400

    fix some problems

--- Microservices/microservicesSpring/course/src/main/java/com/microservice/course/client/StudentCLient.java
+++ Microservices/microservicesSpring/course/src/main/java/com/microservice/course/client/StudentCLient.java
@@ -6,7 +6,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
-@FeignClient(name = "msvc-student", url = "localhost:8080/api/student")
+@FeignClient(name = "${feign.clients.msvc-student.name}", url = "${feign.clients.msvc-student.url}")
+//@FeignClient(name = "msvc-student", url = "localhost:8998/api/student")
 public interface StudentCLient {
     @GetMapping("/search-by-course/{idCourse}")
     List<?> findAllStudentByCourse(@PathVariable Long idCourse);
